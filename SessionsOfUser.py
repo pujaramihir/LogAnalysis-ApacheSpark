0241d3806b70ac730a0bcb7b7bf83a7d30ecf30d
Mihir Pujara <mihir@mihir-laptop>
2017-03-25 21:14:53 -0400

    Second question final code

--- SessionsOfUser.py
+++ SessionsOfUser.py
@@ -1,9 +1,16 @@
 from pyspark import SparkConf,SparkContext
+from operator import add
 import os
 import sys
+import re
 
 def matchToUser(logString):
-    return logString[1]
+    p = re.compile('( Starting Session [0-9]+ of user achille.)+')
+    m = p.match(logString[3])
+    if m:
+        return "True"
+    else:
+        return "False"
 
 conffiguration = SparkConf().setMaster("local[*]").setAppName("LogAnalysis")
 spark = SparkContext(conf = conffiguration)
@@ -14,8 +21,22 @@ arguments = sys.argv
 if len(arguments) == 3:  
     firstHost = spark.textFile(str(os.getcwd())+"/"+str(arguments[1])+"/")
     secondHost = spark.textFile(str(os.getcwd())+"/"+str(arguments[2])+"/")
-    firstResult = firstHost.map(lambda l:l.split(":")).map()
+    firstMapResult = firstHost.map(lambda l:l.split(":")).map(matchToUser).map(lambda l: (l,1))
+    firstResult = firstMapResult.reduceByKey(add).collect()
     
-    print firstResult.collect()
+    secondMapResult = secondHost.map(lambda l:l.split(":")).map(matchToUser).map(lambda l: (l,1))
+    secondResult = secondMapResult.reduceByKey(add).collect()
+    
+    
+    print "* Q2: sessions of user 'achille'"
+    for (key,value) in firstResult:
+        if key == 'True':
+            print "  + "+str(arguments[1])+": "+str(value)
+    
+    for (key,value) in secondResult:
+        if key == 'True':
+            print "  + "+str(arguments[2])+": "+str(value)
+    
+    
 else:
     print "Invalid arguments"
